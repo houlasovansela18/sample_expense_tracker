bc135796ad2b8184f59848b73325a88e71a66f2c
Houla Sovansela <houlasovansela18@kit.edu.kh>
2021-12-26 20:52:27 +0700

    completed-front-end

--- src/popup.jsx
+++ src/popup.jsx
@@ -7,21 +7,22 @@ function history() {
                     {"description":"buying shoes","amount": -500,"date":"19 Dec 2021"}, 
                     {"description":"buy tv","amount": -2000,"date":"27 Dec 2021"}
                 ]
-    const historyList = history.map(history => <li className="border-bottom border-dark">
-        <div className="clearfix">
-            <div className="float-start">{history.description}</div>
-            <div className="float-end"> 
-                {history.amount} $
-                <span className="ms-3 fs-6" type="button">X</span>
+    const historyList = history.map((history,index) => 
+        <li className="list-group-item bg-transparent p-0" id={index + 1}>
+            <div className="clearfix">
+                <div className="float-start text-light">{history.description}</div>
+                <div className="float-end text-light"> 
+                    {history.amount} $
+                    <span className="ms-3 fs-6 text-light" type="button"><i class="fas fa-times-circle"></i></span>
+                </div>
             </div>
-        </div>
-        <div>
-            {history.date}
-        </div>
+            <div className="text-light">
+                {history.date}
+            </div>
         </li>)
     
     return (
-        <ul className="list-group">{historyList}</ul>
+        <ul className="list-group list-group-flush">{historyList}</ul>
     )
 }
 
@@ -32,7 +33,7 @@ function Navbar(){
             <div className="container px-3">
                 <div className="my-2 clearfix">
                     <div className="float-start">
-                        <span className="fs-4">Expense Tracker</span>
+                        <span className="fs-4 text-light">BALANCE</span>
                     </div>
                     <div className="float-end">
                         <a className="bg-transparent" href="#">
@@ -40,11 +41,10 @@ function Navbar(){
                         </a> 
                     </div>
                 </div>
-                <div className="py-2">
-                    <div className="text-center fs-6 fw-light">BALANCE</div>
-                    <div className="text-center display-2" id="balance">$ 400.00</div>
+                <div className="pb-2">
+                    <div className="text-center display-2 text-light fw-bold" id="balance">$ 400.00</div>
                 </div>
-                <div className="text-center py-4 px-0 container overflow-hidden rounded-3 bg-dark">
+                <div className="text-center py-4 px-0 container overflow-hidden rounded-3 bg-black">
                     <div className="row gx-0">
                         <div className="col">
                             <div className="">
@@ -53,37 +53,37 @@ function Navbar(){
                         </div>
                         <div className="col">
                             <div className="">
-                                <div class="text-white text-center fs-2 fw-bold" id="expense">$ 0.00</div>
+                                <div class="text-danger text-center fs-2 fw-bold" id="expense">$ 0.00</div>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className="container bg-transparent py-2 clearfix">
                         <div className="float-start">
-                            <label className="fs-4">HISTORY</label>
+                            <label className="fs-4 text-light">HISTORY</label>
                         </div>
                         <div className="float-end fw-lighter">
-                            <span type="button">See all</span>
+                            <span className="text-light" type="button">See all</span>
                         </div>
                 </div>
                 <div className="container py-2">
                     {history()}
                 </div>
                 <div className="container py-2">
-                    <label className="fs-4">ADD TRANSACTION</label>
+                    <label className="fs-4 text-light">ADD TRANSACTION</label>
                 </div>
                 <div className="container">
                     <div class="my-2 input-group">
-                        <input type="text" class="form-control" aria-label="description" placeholder="description"></input>
+                        <input type="text" class="border-0 form-control  bg-black text-light" aria-label="description" placeholder="description" id="description"></input>
                     </div>
                     <div class="my-2 input-group">
-                        <input type="text" class="form-control" aria-label="Dollar amount (with dot and two decimal places)" placeholder="amount"></input>
-                        <span class="input-group-text">$</span>
+                        <input type="number" class="border-0 form-control bg-black text-light" aria-label="Dollar amount (with dot and two decimal places)" placeholder="amount" id="amount"></input>
+                        <span class="border-0 input-group-text bg-black text-light">$</span>
                     </div>
                 </div>
                 <div class="container my-3 clearfix">
-                    <span type="button" class="btn btn-success float-start">INCOME</span>
-                    <span type="button" class="btn btn-danger float-end">EXPENSE</span>
+                    <span type="button" class="btn bg-black float-start text-light">INCOME</span>
+                    <span type="button" class="btn bg-danger float-end text-light">EXPENSE</span>
                 </div>
             </div>
         </>
